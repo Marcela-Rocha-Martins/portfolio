676fce5577038360d791c8c771754562c568d038
marcela rocha <marcelarochamartins@gmail.com>
2023-09-08 17:25:52 +0200

    windows behaviour

--- src/components/Shortcut.jsx
+++ src/components/Shortcut.jsx
@@ -3,16 +3,34 @@ import React from "react";
 const Shortcut = ({ page, activeWindows, setActiveWindows }) => {
 
   function openWindow() {
+
+    //function to set the logic behind the window views
+
     const clickedWindows = [...activeWindows];
-    clickedWindows.push(page);
+
+    let index = activeWindows.indexOf(page);
+
+    if (index < 0) {
+
+      clickedWindows.push(page);
+
+    } else {
+
+      clickedWindows.splice(index, 1);
+
+      clickedWindows.push(page);
+    }
+
     setActiveWindows(clickedWindows);
+    console.log(clickedWindows, "2) clickedWindows com unshift");
   }
+
+
 
   return (
     <button onClick={openWindow} className="folder">
       {page.name}
     </button>
-    
   );
 };
 
